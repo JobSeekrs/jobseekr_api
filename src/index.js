93d196798bcc64369a4a4a54a80102f47e47b548
Jeffrey Ueo <jeffreyueo@gmail.com>
2017-12-05 19:51:30 -0800

    added routes and controller for search function and github api

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import bodyparser from 'body-parser';
-import path from 'path';
 import dotenv from 'dotenv';
+import Router from './router/index';
 
-dotenv.config();
+// dotenv.config();
 const app = express();
-const port = process.env.PORT;
+// const port = process.env.PORT;
 
 app.use(bodyparser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use('/', Router);
 
-app.listen(port, () => {
-  console.log(`rest-server listening on port ${port}`);
+
+// app.listen(port, () => {
+//   console.log(`rest-server listening on port ${port}`);
+// });
+
+app.listen(3100, () => {
+  console.log('rest-server listening to port 3100');
 });
 
 module.exports = app;
