4d17f562797d11d38a2f0ce0ef9b1c6bed5a7980
Brent Eickhoff <dev@eickhoffs.net>
2017-12-06 15:23:20 -0800

    Database schema build and seed on startup, strated api routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import bodyparser from 'body-parser';
-import path from 'path';
 import dotenv from 'dotenv';
+import router from './router';
+import { initializeDb } from './database';
+
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
 app.use(bodyparser.json());
+// app.use(router);
 
 app.listen(port, () => {
   console.log(`rest-server listening on port ${port}`);
+});
+
+initializeDb((err) => {
+  if (err) {
+    console.error('Error initializing DB:', err);
+  } else {
+    console.log('Database initialized');
+  }
 });
 
 module.exports = app;
