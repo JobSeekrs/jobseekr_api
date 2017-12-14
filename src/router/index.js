60cfafba551184045d6ad8bc33f16a9dc4e50d94
Jeffrey Ueo <jeffreyueo@gmail.com>
2017-12-13 20:00:53 -0800

    cleaned up index.js

--- src/router/index.js
+++ src/router/index.js
@@ -14,70 +14,6 @@ router.route('/signup')
 router.route('/login')
   .post(controller.user.login);
 
-// router.route('/github')
-//   .post(controller.github.post);
-
-// router.route('/user')
-//   .get(controller.user.get);
-//   // .post()
-//   // .put()
-//   // .delete();
-
-// router.route('/company')
-//   .get(controller.company.get)
-//   .post(controller.company.post)
-//   //   .put()
-//   //   .delete();
-//   .get(validateJWT, controller.company.get);
-// //   .post()
-//   .get(controller.company.get)
-//   .post(controller.company.post);
-// //   .put()
-// //   .delete();
-
-// router.route('/company/search')
-// .post(controller.company.searchPost);
-
-// router.route('/contact')
-//   .get(controller.contact.get)
-//   .post(controller.contact.post)
-//   //   .put()
-//   //   .delete();
-
-// router.route('/job')
-//   .get(controller.job.get)
-//   .post(controller.job.post)
-//   //   .put()
-//   //   .delete();
-
-// router.route('/dashboard')
-//   .get(controller.dashboard.get);
-//   .get(validateJWT, controller.job.get);
-// //   .post() 
-//   .get(controller.job.get)
-//   .post(controller.job.post); 
-// //   .put()
-// //   .delete();
-// router.route('/job/search')
-// .post(controller.job.searchPost);
-
-// router.route('/event')
-//   .get(controller.event.get)
-//   //   .post()
-//   //   .put()
-//   //   .delete();
-
-// router.route('/resource')
-//   .get(controller.resource.get);
-//   //   .post()
-//   //   .put()
-//   //   .delete();
-router.route('/signup')
-.post(controller.user.signup);
-
-router.route('/login')
-.post(controller.user.login);
-
 router.route('/github')
   .post(validateJWT, controller.github.post);
 
@@ -107,13 +43,14 @@ router.route('/job')
 //   .put()
 //   .delete();
 
+router.route('/job/search')
+.post(controller.job.searchPost);
+
 router.route('/dashboard')
   .get(validateJWT, controller.dashboard.get);
 //   .post()
 //   .put()
 //   .delete();
-router.route('/job/search')
-.post(controller.job.searchPost);
 
 router.route('/event')
   .get(validateJWT, controller.event.get)
