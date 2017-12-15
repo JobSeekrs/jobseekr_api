2c30aa8279dc0d160d901ac4d843f83f4ada6b1f
Brent Eickhoff <brenteickhoff@gmail.com>
2017-12-14 17:30:55 -0800

    Fix routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,102 +1,62 @@
-import { log, debug } from '../'
+
 import express from 'express';
+import { log, debug } from '../';
+import auth from '../helper/authentication';
 import controller from '../controller';
-import authentication from '../helper/authentication';
+
 
 const router = express.Router();
-// const validateJWT = authentication.validateJWT;
+const validateJWT = auth.validateJWT;
 
-// debug('FUNCTION', authentication.validateJWT)
+router.route('/signup')
+  .post(controller.user.signup);
 
-// router.route('/signup')
-//   .post(controller.user.signup);
-
-// router.route('/login')
-//   .post(controller.user.login);
-
-// router.route('/github')
-//   .post(validateJWT, controller.github.post);
-
-// router.route('/user')
-//   .get(validateJWT, controller.user.get);
-//   // .post()
-//   // .put()
-//   // .delete();
-
-// router.route('/company')
-//   .get(validateJWT, controller.company.get)
-//   .post(controller.company.post)
-// //   .put()
-// //   .delete();
-
-// router.route('/contact')
-//   .get(validateJWT, controller.contact.get)
-//   .post(controller.contact.post)
-// //   .put()
-// //   .delete();
-
-// router.route('/job')
-//   .get(validateJWT, controller.job.get)
-//   .post(controller.job.post)
-// //   .put()
-// //   .delete();
-
-// router.route('/event')
-//   .get(validateJWT, controller.event.get)
-// //   .post()
-// //   .put()
-// //   .delete();
-
-// router.route('/resource')
-//   .get(validateJWT, controller.resource.get);
-// //   .post()
-// //   .put()
-// //   .delete();
+router.route('/login')
+  .post(controller.user.login);
 
 router.route('/github')
-  .post(controller.github.post);
+  .post(validateJWT, controller.github.post);
 
 router.route('/user')
-  .get(controller.user.get);
-  // .post()
-  // .put()
-  // .delete();
+  .get(validateJWT, controller.user.get);
+// .post()
+// .put()
+// .delete();
+//   .get(validateJWT, controller.event.get)
 
 router.route('/company')
-  .get(controller.company.get)
-  .post(controller.company.post)
-  //   .put()
-  //   .delete();
+//   .put()
+//   .delete();
 
 router.route('/contact')
-  .get(controller.contact.get)
-  .post(controller.contact.post)
-  //   .put()
-  //   .delete();
+  .get(validateJWT, controller.contact.get)
+  .post(validateJWT, controller.contact.post);
+//   .put()
+//   .delete();
 
 router.route('/job')
-  .get(controller.job.get)
-  .post(controller.job.post)
-  //   .put()
-  //   .delete();
+  .get(validateJWT, controller.job.get)
+  .post(validateJWT, controller.job.post);
+//   .put()
+//   .delete();
 
 router.route('/dashboard')
-  .get(controller.dashboard.get)
-//   .post() 
+  .get(validateJWT, controller.dashboard.get);
+//   .post()
 //   .put()
 //   .delete();
 
 router.route('/event')
-  .get(controller.event.get)
-  .post(controller.event.post)
-  //   .put()
-  //   .delete();
+  .get(validateJWT, controller.event.get)
+  .post(validateJWT, controller.event.post);
+// .put()
+// .delete();
 
 router.route('/resource')
-  .get(controller.resource.get);
-  //   .post()
-  //   .put()
-  //   .delete();
+  .get(validateJWT, controller.resource.get);
+//   .post()
+//   .put()
+//   .delete();
 
 export default router;
 
