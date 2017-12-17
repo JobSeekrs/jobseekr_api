06cad29d7bf08149ffd61b95dc79cc844fff2971
Jamie <jamie.lo112@gmail.com>
2017-12-16 16:25:12 -0800

    [fix]
    remove console.log

--- src/controller/local/jobDetailsNotes/jobDetailsNotes.js
+++ src/controller/local/jobDetailsNotes/jobDetailsNotes.js
@@ -4,7 +4,6 @@ const TYPE = 'jobDetailsNotes';
 
 export default  {
   put: (req, res) => {
-    console.log('im in controller', req.body)
     db.model[TYPE].put(req.body, (err, data) => {
       res.status(200).send(JSON.stringify(data));
     });
