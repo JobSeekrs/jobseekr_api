1376f0d295e8f50e2d6921ce82f3607822021508
Brent Eickhoff <brenteickhoff@gmail.com>
2017-12-18 10:53:46 -0800

    Rebase

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   // res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", 
-  "Authorization, UserId, Origin, X-Requested-With, Content-Type, Accept");
+  "token, userid, Authorization, UserId, Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
